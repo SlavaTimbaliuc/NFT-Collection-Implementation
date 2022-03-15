32f0e65a7a701019d7ff6526f5bc97342370b60c
Slava Timbaliuc <slava.timbaliuc@gmail.com>
2022-03-15 14:46:00 -0400

    initia commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2F2dFB31bD1B170870Ae0fcdBE185c5572344E4F";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
